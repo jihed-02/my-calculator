d8bda36891f08969e11ad718bd1511b8f31096f9
Jihed Ghanem <jihedghanem02@gmail.com>
2026-03-09 17:02:37 +0300

    add pow function

--- main.py
+++ main.py
@@ -12,11 +12,16 @@ def realDivi(a, b):
     return a / b
 
 
+def pow(a, b):
+    return a ** b
+
+
 def menu():
     print("Calculator")
     print("1 - Sum")
     print("2 - Subtraction")
     print("3 - Real Division")
+    print("4 - Power")
 
     choice = input("Choose an operation: ")
 
@@ -29,6 +34,8 @@ def menu():
         print("Result:", sub(a, b))
     elif choice == "3":
         print("Result:", realDivi(a, b))
+    elif choice == "4":
+        print("Result:", pow(a, b))
     else:
         print("Invalid choice")
 
