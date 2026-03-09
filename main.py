5dc7153d8e5468901c8fac04e59f1521ac2e797c
Jihed Ghanem <jihedghanem02@gmail.com>
2026-03-09 17:04:52 +0300

    Revert "add pow function"
    This reverts commit d8bda36891f08969e11ad718bd1511b8f31096f9.

--- main.py
+++ main.py
@@ -12,16 +12,11 @@ def realDivi(a, b):
     return a / b
 
 
-def pow(a, b):
-    return a ** b
-
-
 def menu():
     print("Calculator")
     print("1 - Sum")
     print("2 - Subtraction")
     print("3 - Real Division")
-    print("4 - Power")
 
     choice = input("Choose an operation: ")
 
@@ -34,8 +29,6 @@ def menu():
         print("Result:", sub(a, b))
     elif choice == "3":
         print("Result:", realDivi(a, b))
-    elif choice == "4":
-        print("Result:", pow(a, b))
     else:
         print("Invalid choice")
 
