bd9f63a035ae7969d80e9677537ef5185d049b4d
Jihed Ghanem <jihedghanem02@gmail.com>
2026-03-09 16:57:55 +0300

    add sub function

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ def sum(a, b):
     return a + b
 
 
+def sub(a, b):
+    return a - b
+
+
 def menu():
     print("Calculator")
     print("1 - Sum")
+    print("2 - Subtraction")
 
     choice = input("Choose an operation: ")
 
+    a = float(input("Enter a: "))
+    b = float(input("Enter b: "))
+
     if choice == "1":
-        a = float(input("Enter a: "))
-        b = float(input("Enter b: "))
         print("Result:", sum(a, b))
+    elif choice == "2":
+        print("Result:", sub(a, b))
     else:
         print("Invalid choice")
 
